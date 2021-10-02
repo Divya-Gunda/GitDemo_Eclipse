dc529f35e948d8a0d10ea9aae3facd6f16b74dbd
Divya-Gunda <91668781+Divya-Gunda@users.noreply.github.com>
2021-10-02 08:45:49 +0530

    First file in git demo using eclipse in master branch

---
+++ src/sample.java
@@ -0,0 +1,12 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+
+		String str= "Hello";
+		int a=7;
+		System.out.println("The string along with number "+str+" "+a);
+	}
+
+}
+
