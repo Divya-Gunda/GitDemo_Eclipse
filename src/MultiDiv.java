f8139e7e815d526a5ae35c5a4b4950881647a2bf
Divya-Gunda <91668781+Divya-Gunda@users.noreply.github.com>
2021-10-04 12:19:55 +0530

    multiplication and division of two numbers

---
+++ src/MultiDiv.java
@@ -0,0 +1,11 @@
+
+public class MultiDiv {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		System.out.println("The product of two numbers -> "+a+"*"+b+"="+(a*b));
+		System.out.println("The division of two numbers -> "+a+"/"+b+"="+(a/b));
+
+	}
+
+}
